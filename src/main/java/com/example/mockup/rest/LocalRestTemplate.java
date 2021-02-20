bf468f0daca6e09f9dc4a2c96e46ae07b9365d67
admin <rong@agile-solutions.ch>
2021-02-20 12:18:47 +0100

    add split

--- src/main/java/com/example/mockup/rest/LocalRestTemplate.java
+++ src/main/java/com/example/mockup/rest/LocalRestTemplate.java
@@ -10,14 +10,24 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class LocalRestTemplate extends RestTemplate {
 
-    private ConcurrentHashMap<String, String> payload = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, String> payloads = new ConcurrentHashMap();
 
     @Override
     public <T> ResponseEntity exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables) throws RestClientException {
 
+        String id = url.split("/")[3];
 
-        return ResponseEntity.accepted().body(payload.get(""));
+        return ResponseEntity.accepted().body(payloads.get(""));
 
         //return super.exchange(url, method, requestEntity, responseType, uriVariables);
+    }
+
+    public void storePayload(String id, String payload) {
+
+        if (payloads.contains(id)) {
+            payloads.replace(id, payload);
+        } else {
+            payloads.put(id, payload);
+        }
     }
 }
