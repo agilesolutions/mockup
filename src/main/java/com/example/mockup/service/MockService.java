e17f2367c8a5f9b11e0cb6debd3b0361248c3e8b
admin <rong@agile-solutions.ch>
2021-02-20 14:33:34 +0100

    controller

--- src/main/java/com/example/mockup/service/MockService.java
+++ src/main/java/com/example/mockup/service/MockService.java
@@ -3,6 +3,7 @@ package com.example.mockup.service;
 import com.example.mockup.rest.LocalRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
